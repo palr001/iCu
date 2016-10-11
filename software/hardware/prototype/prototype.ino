88053fef3994848f86a7a290c8104962c7a744f6
Wilco Wijdenes <wilco.wijdenes@hva.nl>
2016-10-11 19:14:36 +0200

    Prototype wifi works in theory

--- software/hardware/prototype/prototype.ino
+++ software/hardware/prototype/prototype.ino
@@ -1,8 +1,9 @@
 #include <OpenWiFi.h>
 #include <ESP8266HTTPClient.h>
-OpenWiFi hotspot;
 
+OpenWiFi hotspot;
 int pushButton = D1;
+int oldButtonState = LOW;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -16,36 +17,33 @@ void setup() {
   hotspot.begin("HomeNetwork", "HomePassword");
 }
 
-int oldstate = 0;
-
 // the loop routine runs over and over again forever:
 void loop() {
+  
   // read the input pin:
   int buttonState = digitalRead(pushButton);
-  // print out the state of the button:
 
-  delay(1);        // delay in between reads for stability
-
-  if (oldstate != buttonState && buttonState == 1) {
-    HTTPClient http;
-    http.begin("http://178.62.233.141/add_entry.php");
-    uint16_t httpCode = http.GET();
-    Serial.println("posted");
-    String response;
-
-    if (httpCode == 200) {
-      response = http.getString();
-      Serial.println(response);
+  delay(1); // delay in between reads for stability
+  if (oldButtonState != buttonState) {
+    if(buttonState == HIGH) {
+      Serial.println("Send request to server");
+      
+      HTTPClient http;
+      http.begin("http://178.62.233.141/add_entry.php");
+      uint16_t httpCode = http.GET();
+ 
+      if (httpCode == 200) {
+        String response;
+        response = http.getString();
+        Serial.println(response);
+      } else {
+        ESP.reset();
+      }
+      
+      http.end();
+      oldButtonState = buttonState;
+    } else {
+      oldButtonState = LOW;
     }
-    else
-      ESP.reset();
-
-    http.end();
-
-    oldstate = buttonState;
   }
 }
-
-
-
-
