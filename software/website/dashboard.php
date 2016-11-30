17867e97dd017b2dd7f92c3f2290cfb5c5f0f7c2
Wilco Wijdenes <wilco.wijdenes@hva.nl>
2016-11-30 14:12:58 +0100

    Resolved delete bug in dashboard

--- software/website/dashboard.php
+++ software/website/dashboard.php
@@ -1,17 +1,26 @@
 <?php
   require_once('check.php');
+    $incoming_stmt = $pdo->prepare("SELECT * FROM device_configuration WHERE target_device_id = ?");
+    if(!$incoming_stmt->execute([$_GET['d']])) {
+      echo 'Something has gone wrong!';
+      exit;
+    }
+    $incoming_count = $incoming_stmt->rowCount();
+    $outgoing_stmt = $pdo->prepare("SELECT * FROM device_configuration WHERE device_id = ?");
+    if(!$stmt->execute([$_GET['d']])) {
+      echo 'Something has gone wrong!';
+      exit;
+    }
+    $outgoing_count = $outgoing_stmt->rowCount();
     $filter_type = 'out';
     $filter_column = 'device_id';
     $display_column = 'target_device_id';
+    $stmt = $outgoing_stmt;
     if(isset($_GET['f']) && $_GET['f'] == 'in') {
       $filter_type = 'in';
       $filter_column = 'target_device_id';
       $display_column = 'device_id';
-    }
-    $stmt = $pdo->prepare("SELECT * FROM device_configuration WHERE " . $filter_column . " = ?");
-    if(!$stmt->execute([$_GET['d']])) {
-      echo 'Something has gone wrong!';
-      exit;
+      $stmt = $incoming_stmt;
     }
 ?>
 <!doctype html>
@@ -30,11 +39,11 @@
       <button type="submit" class="circle-button" form="device" formaction="device_configuration.php">+</button>
     </div>
     <div id="filter" class="middle-container">
-      <button class="filter-button" name="f" value="out" form="device" formaction="#filter" <?php echo isset($_GET['f']) ? ($_GET['f'] == 'out' ? 'disabled' : '') : 'disabled'; ?>>
-        <img src="img/icon_outgoing.png" width="17" height="17" class="button-image"> (<?php echo $stmt->rowCount(); ?>)
+      <button class="filter-button <?php echo ($outgoing_count + $incoming_count == 0 ? 'left-br' : 'left-tbr'); ?>" title="Outgoing" name="f" value="out" form="device" formaction="#filter" <?php echo isset($_GET['f']) ? ($_GET['f'] == 'out' ? 'disabled' : '') : 'disabled'; ?>>
+        <img src="img/icon_outgoing.png" width="17" height="17" class="button-image"> (<?php echo $outgoing_count; ?>)
       </button>
-      <button class="filter-button" name="f" value="in" form="device" formaction="#filter" <?php echo isset($_GET['f']) ? ($_GET['f'] == 'in' ? 'disabled' : '') : ''; ?>>
-        <img src="img/icon_incoming.png" width="17" height="17" class="button-image"> (<?php echo $stmt->rowCount(); ?>)
+      <button class="filter-button <?php echo ($outgoing_count + $incoming_count == 0 ? 'right-br' : 'right-tbr'); ?>"" title="Incoming" name="f" value="in" form="device" formaction="#filter" <?php echo isset($_GET['f']) ? ($_GET['f'] == 'in' ? 'disabled' : '') : ''; ?>>
+        <img src="img/icon_incoming.png" width="17" height="17" class="button-image"> (<?php echo $incoming_count; ?>)
       </button>
     </div>
     <table class="full-width">
@@ -61,19 +70,23 @@
                       echo '</td>
                       <td>
                         <div class="vertical-gap-5 small-cell">
-                          <form action="api.php">
-                            <input type="hidden" name="r" value="/dashboard.php?d=' . $row['target_device_id'] . '&f=' . $filter_type . '#filter">
-                            <input type="hidden" name="d" value="' . $row['target_device_id'] . '">
-                            <input type="hidden" name="td" value="' . $row['device_id'] . '">';
+                          <form action="api.php">';
                             if ($filter_type == 'in') {
-                              echo '<input type="hidden" name="b" value="' . ($row['blacklist'] == 1 ? 0 : 1) . '">';
+                              echo '<input type="hidden" name="d" value="' . $row['target_device_id'] . '">
+                                    <input type="hidden" name="td" value="' . $row['device_id'] . '">
+                                    <input type="hidden" name="b" value="' . ($row['blacklist'] == 1 ? 0 : 1) . '">
+                                    <input type="hidden" name="r" value="/dashboard.php?d=' . $row['target_device_id'] . '&f=' . $filter_type . '#filter">';
                               if($row['blacklist'] == 1) {
                                   echo '<button type="submit" name="t" value="bdc" class="add icon" title="Remove from blacklist"></button>';
                               } else {
                                   echo '<button type="submit" name="t" value="bdc" class="delete icon" title="Blacklist"></button>';
                               }
                             } else {
-                              echo '<button type="submit" name="t" value="rdc" class="delete icon" title="Delete"></button>';
+                              echo '
+                                <input type="hidden" name="d" value="' . $row['device_id'] . '">
+                                  <input type="hidden" name="td" value="' . $row['target_device_id'] . '">
+                                    <input type="hidden" name="r" value="/dashboard.php?d=' . $row['device_id'] . '&f=' . $filter_type . '#filter">
+                                  <button type="submit" name="t" value="rdc" class="delete icon" title="Delete"></button>';
                             }
 
                         echo '</form>
