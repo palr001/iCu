373b0088b6d3fac69f38293623add66898226eb5
Bas Pijls <bpijls@gmail.com>
2016-12-19 00:54:35 +0100

    Adds DEBUG_MODE constant in config.h.

--- software/hardware/prototype/config.h
+++ software/hardware/prototype/config.h
@@ -1,6 +1,7 @@
 #ifndef _CONFIG_H 
 #define _CONFIG_H
 
+#define DEBUG_MODE
 #define LED_COUNT    6
 #define LED_PIN     D2   
 #define BUTTON_PIN  D1
@@ -8,7 +9,7 @@
 #define FADEIN_DELAY 5
 #define FADEOUT_DELAY 8
 
-
+#define PROJECT_SHORT_NAME "ICU"
 #define REQUEST_DELAY 2000
 
 #define SERVER_URL "http://thingscon16.futuretechnologies.nl"
