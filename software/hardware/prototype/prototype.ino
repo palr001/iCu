58457697faf48c4973c9d42e2c75a2221e233e01
Bas Pijls <bpijls@gmail.com>
2016-12-19 00:54:42 +0100

    Refactors all serial output to printDebugMessage

--- software/hardware/prototype/prototype.ino
+++ software/hardware/prototype/prototype.ino
@@ -2,7 +2,7 @@
 #include <Servo.h>
 #include <DNSServer.h>
 #include <ESP8266WiFi.h>
-#include <WiFiManager.h> 
+#include <WiFiManager.h>
 #include "SpringyValue.h"
 #include "config.h"
 #include "WS2812_util.h"
@@ -14,7 +14,13 @@ int oscillationTime = 500;
 String chipID;
 String serverURL = SERVER_URL;
 
-void setup() 
+void printDebugMessage(String message) {
+#ifdef DEBUG_MODE
+  Serial.println(String(PROJECT_SHORT_NAME) + ": "+ message);
+#endif
+}
+
+void setup()
 {
   chipID = generateChipID();
   strip.begin();
@@ -23,29 +29,30 @@ void setup()
   Serial.begin(115200);
 
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  printDebugMessage(String("Last 2 bytes of chip ID: ") + chipID);
+
   int counter = 0;
-  while(digitalRead(BUTTON_PIN) == LOW)
+  while (digitalRead(BUTTON_PIN) == LOW)
   {
     counter++;
     delay(10);
 
-    if(counter > 500)
+    if (counter > 500)
     {
       wifiManager.resetSettings();
-      Serial.println("Remove all wifi settings!");
-      setAllPixels(255,0,0,1.0);
-      fadeBrightness(255,0,0,1.0);
+      printDebugMessage("Remove all wifi settings!");
+      setAllPixels(255, 0, 0, 1.0);
+      fadeBrightness(255, 0, 0, 1.0);
       ESP.reset();
     }
   }
-  
+
   delay(1000);  
-  Serial.print(String("\nLast 2 bytes of chip ID: ") + chipID);
-  
-  String configSSID = String(CONFIG_SSID) + "_" + chipID;  
-  setAllPixels(0,255,255,1.0);
+  String configSSID = String(CONFIG_SSID) + "_" + chipID;
+  setAllPixels(0, 255, 255, 1.0);
   wifiManager.autoConnect(configSSID.c_str());
-  fadeBrightness(0,255,255,1.0);
+  fadeBrightness(0, 255, 255, 1.0);
   myServo.attach(D7);
 }
 
@@ -53,24 +60,24 @@ void setup()
 void oscillate(float springConstant, float dampConstant, int c)
 {
   SpringyValue spring;
-  
+
   byte red = (c >> 16) & 0xff;
   byte green = (c >> 8) & 0xff;
   byte blue = c & 0xff;
-  
+
   spring.c = springConstant;
   spring.k = dampConstant / 100;
   spring.perturb(255);
 
   //Start oscillating
-  for(int i = 0; i < oscillationTime; i++)
+  for (int i = 0; i < oscillationTime; i++)
   {
     spring.update(0.01);
     setAllPixels(red, green, blue, abs(spring.x) / 255.0);
-    myServo.write(90 + spring.x/4); 
+    myServo.write(90 + spring.x / 4);
 
     //Check for button press
-    if(digitalRead(BUTTON_PIN) == LOW)
+    if (digitalRead(BUTTON_PIN) == LOW)
     {
       //Fade the current color out
       fadeBrightness(red, green, blue, abs(spring.x) / 255.0);
@@ -81,17 +88,17 @@ void oscillate(float springConstant, float dampConstant, int c)
   fadeBrightness(red, green, blue, abs(spring.x) / 255.0);
 }
 
-void loop() 
+void loop()
 {
   //Check for button press
-  if(digitalRead(BUTTON_PIN) == LOW)
-  {    
+  if (digitalRead(BUTTON_PIN) == LOW)
+  {
     sendButtonPress();
     delay(250);
   }
 
   //Every requestDelay, send a request to the server
-  if(millis() > oldTime + REQUEST_DELAY)
+  if (millis() > oldTime + REQUEST_DELAY)
   {
     requestMessage();
     oldTime = millis();
@@ -100,50 +107,50 @@ void loop()
 
 void sendButtonPress()
 {
-    Serial.println("Sending button press to server");
-    HTTPClient http;
-    http.begin(serverURL + "/api.php?t=sqi&d=" + chipID);
-    uint16_t httpCode = http.GET();      
-    http.end();
+  printDebugMessage("Sending button press to server");
+  HTTPClient http;
+  http.begin(serverURL + "/api.php?t=sqi&d=" + chipID);
+  uint16_t httpCode = http.GET();
+  http.end();
 }
 
 void requestMessage()
 {
-  Serial.println("Sending request to server");
+  printDebugMessage("Sending request to server");
   hideColor();
-      
+
   HTTPClient http;
   http.begin(serverURL + "/api.php?t=gqi&d=" + chipID + "&v=2");
   uint16_t httpCode = http.GET();
 
-  if (httpCode == 200) 
+  if (httpCode == 200)
   {
     String response;
     response = http.getString();
     //Serial.println(response);
 
-    if(response == "-1")
+    if (response == "-1")
     {
-      Serial.println("There are no messages waiting in the queue");
+      printDebugMessage("There are no messages waiting in the queue");
     }
     else
     {
       //Get the indexes of some commas, will be used to split strings
       int firstComma = response.indexOf(',');
-      int secondComma = response.indexOf(',', firstComma+1);
-      int thirdComma = response.indexOf(',', secondComma+1);
-      
-      //Parse data as strings
-      String hexColor = response.substring(0,7);
-      String springConstant = response.substring(firstComma+1,secondComma);
-      String dampConstant = response.substring(secondComma+1,thirdComma);;
-      String message = response.substring(thirdComma+1,response.length());;
+      int secondComma = response.indexOf(',', firstComma + 1);
+      int thirdComma = response.indexOf(',', secondComma + 1);
 
-      Serial.println("Message received from server: \n");
-      Serial.println("Hex color received: " + hexColor);
-      Serial.println("Spring constant received: " + springConstant);
-      Serial.println("Damp constant received: " + dampConstant);
-      Serial.println("Message received: " + message);
+      //Parse data as strings
+      String hexColor = response.substring(0, 7);
+      String springConstant = response.substring(firstComma + 1, secondComma);
+      String dampConstant = response.substring(secondComma + 1, thirdComma);;
+      String message = response.substring(thirdComma + 1, response.length());;
+
+      printDebugMessage("Message received from server: \n");
+      printDebugMessage("Hex color received: " + hexColor);
+      printDebugMessage("Spring constant received: " + springConstant);
+      printDebugMessage("Damp constant received: " + dampConstant);
+      printDebugMessage("Message received: " + message);
 
       //Extract the hex color and fade the led strip
       int number = (int) strtol( &response[1], NULL, 16);
@@ -152,9 +159,9 @@ void requestMessage()
   }
   else
   {
-    ESP.reset(); 
+    ESP.reset();
   }
-    
+
   http.end();
 }
 
