d2eb8f5bcbb42af2e595dc9890dbad8dcc3f03be
Bas Pijls <bpijls@gmail.com>
2016-12-19 14:12:14 +0100

    Alters SQL to only create the tables

--- SQL/201611062250.sql
+++ SQL/201611062250.sql
@@ -1,68 +1,103 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 4.6.0
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 19, 2016 at 01:18 PM
+-- Server version: 5.6.30
+-- PHP Version: 5.6.20-pl0-gentoo
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema internet_of_things_workshop
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema internet_of_things_workshop
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `internet_of_things_workshop` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `internet_of_things_workshop` ;
-
--- -----------------------------------------------------
--- Table `internet_of_things_workshop`.`device`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `internet_of_things_workshop`.`device` (
-  `id` VARCHAR(4) NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '')
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `internet_of_things_workshop`.`device_configuration`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `internet_of_things_workshop`.`device_configuration` (
-  `device_id` VARCHAR(4) NOT NULL COMMENT '',
-  `target_device_id` VARCHAR(4) NOT NULL COMMENT '',
-  `color` VARCHAR(11) NOT NULL COMMENT '',
-  INDEX `fk_queue_device_idx` (`device_id` ASC)  COMMENT '',
-  INDEX `fk_queue_device1_idx` (`target_device_id` ASC)  COMMENT '',
-  PRIMARY KEY (`target_device_id`, `device_id`)  COMMENT '',
-  CONSTRAINT `fk_queue_device`
-    FOREIGN KEY (`device_id`)
-    REFERENCES `internet_of_things_workshop`.`device` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_queue_device1`
-    FOREIGN KEY (`target_device_id`)
-    REFERENCES `internet_of_things_workshop`.`device` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `zpijlb`
+--
 
--- -----------------------------------------------------
--- Table `internet_of_things_workshop`.`queue`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `internet_of_things_workshop`.`queue` (
-  `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
-  `target_device_id` VARCHAR(4) NOT NULL COMMENT '',
-  `device_id` VARCHAR(4) NOT NULL COMMENT '',
-  PRIMARY KEY (`timestamp`, `target_device_id`, `device_id`)  COMMENT '',
-  INDEX `fk_queue_device_configuration1_idx` (`target_device_id` ASC, `device_id` ASC)  COMMENT '',
-  CONSTRAINT `fk_queue_device_configuration1`
-    FOREIGN KEY (`target_device_id` , `device_id`)
-    REFERENCES `internet_of_things_workshop`.`device_configuration` (`target_device_id` , `device_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `application`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `icu_device`
+--
+
+CREATE TABLE `icu_device` (
+  `id` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `icu_device_configuration`
+--
+
+CREATE TABLE `icu_device_configuration` (
+  `device_id` varchar(4) NOT NULL,
+  `target_device_id` varchar(4) NOT NULL,
+  `color` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `icu_queue`
+--
+
+CREATE TABLE `icu_queue` (
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `target_device_id` varchar(4) NOT NULL,
+  `device_id` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for table `icu_device`
+--
+ALTER TABLE `icu_device`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `icu_device_configuration`
+--
+ALTER TABLE `icu_device_configuration`
+  ADD PRIMARY KEY (`target_device_id`,`device_id`),
+  ADD KEY `fk_queue_device_idx` (`device_id`),
+  ADD KEY `fk_queue_device1_idx` (`target_device_id`);
+
+--
+-- Indexes for table `icu_queue`
+--
+ALTER TABLE `icu_queue`
+  ADD PRIMARY KEY (`timestamp`,`target_device_id`,`device_id`),
+  ADD KEY `fk_queue_device_configuration1_idx` (`target_device_id`,`device_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `icu_device_configuration`
+--
+ALTER TABLE `icu_device_configuration`
+  ADD CONSTRAINT `fk_queue_device` FOREIGN KEY (`device_id`) REFERENCES `icu_device` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_queue_device1` FOREIGN KEY (`target_device_id`) REFERENCES `icu_device` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `icu_queue`
+--
+ALTER TABLE `icu_queue`
+  ADD CONSTRAINT `fk_queue_device_configuration1` FOREIGN KEY (`target_device_id`,`device_id`) REFERENCES `icu_device_configuration` (`target_device_id`, `device_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
