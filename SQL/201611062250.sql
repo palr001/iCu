76632f060a13c91c66868a94aa0526533aaf95f4
Bas Pijls <bpijls@gmail.com>
2016-12-19 14:48:18 +0100

    Removes all specific database information from comments

--- SQL/201611062250.sql
+++ SQL/201611062250.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.6.0
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Dec 19, 2016 at 01:18 PM
--- Server version: 5.6.30
--- PHP Version: 5.6.20-pl0-gentoo
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -16,9 +7,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `zpijlb`
---
 
 -- --------------------------------------------------------
 
